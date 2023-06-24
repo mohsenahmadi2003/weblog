e07fc484f454831045254c6f7b3621f5dbfd14b2
Mohsen <91451917+mohsenahmadi2003@users.noreply.github.com>
2023-06-24 07:13:22 +0330

    create profile model

--- weblog/models.py
+++ weblog/models.py
@@ -8,3 +8,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
