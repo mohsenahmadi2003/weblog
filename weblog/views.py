c252eecdec2cc94860790136008845b7f1a7f060
Mohsen <91451917+mohsenahmadi2003@users.noreply.github.com>
2023-06-24 07:44:02 +0330

    update views

--- weblog/views.py
+++ weblog/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from .models import Post
 from .forms import PostForm, SignUpForm
 
