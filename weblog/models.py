5ae65ee6090b185e0e7937d8ba75d626933eb055
Mohsen <91451917+mohsenahmadi2003@users.noreply.github.com>
2023-06-24 07:12:33 +0330

    create post model

---
+++ weblog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
