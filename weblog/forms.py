f1c32acbfd6074b6e8577bd57ea164dd4906eb8d
Mohsen <91451917+mohsenahmadi2003@users.noreply.github.com>
2023-06-24 07:15:14 +0330

    create forms.py

---
+++ weblog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text']
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2',)
